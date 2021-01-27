e7bf3bfd9292771bae8b66a39170f78f632918ef
Leonardo Vitullo <leonardo.vitullo@gruppofilippetti.it>
2021-01-27 14:05:17 +0100

    Aggiungere i file di progetto.

---
+++ WpfAppMVVM/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfAppMVVM
+{
+    public class PersonViewModel
+    {
+        private IList<Person> _personList;
+        public PersonViewModel()
+        {
+            _personList = new List<Person>()
+        {
+            new Person(){Name="Prabhat", Address="Bangalore"},
+            new Person(){Name="John",Address="Delhi"}
+        };
+        }
+        public IList<Person> Persons
+        {
+            get { return _personList; }
+            set { _personList = value; }
+        }
+        private ICommand mUpdater;
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (mUpdater == null)
+                    mUpdater = new Updater();
+                return mUpdater;
+            }
+            set
+            {
+                mUpdater = value;
+            }
+        }
+    }
+
+}
